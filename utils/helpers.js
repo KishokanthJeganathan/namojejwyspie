2e5a40a25496250cca465192adc0afc7bb233e22
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-12-16 22:29:04 +0100

    Create helpers.js

---
+++ utils/helpers.js
@@ -0,0 +1,41 @@
+import { css } from 'styled-components';
+
+export const setColor = {
+	black: 'black',
+	orange: '#D4A35E',
+	white: 'white'
+};
+
+export const setFontWeight = {
+	thin: '300',
+	normal: '400',
+	bold: '600'
+};
+
+export const setPosition = {
+	center: `display: flex;
+	justify-content:center;
+	align-items:center;`
+};
+
+export const setFontSize = {
+	title: '3rem',
+	smallTitle: '2rem',
+	normal: '1.1rem',
+	normalSmall: '1rem',
+	verySmall: '0.8rem'
+};
+
+const sizes = {
+	large: 1200,
+	desktop: 992,
+	tablet: 768,
+	phone: 576,
+	small: 375
+};
+
+// Iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce((acc, label) => {
+	acc[label] = (...args) => css`@media (min-width: ${sizes[label] / 16}em) {${css(...args)};}`;
+	return acc;
+}, {});
