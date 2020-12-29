ab6a31d4afd9471b107e64044973662e55c90ae0
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-12-29 14:01:40 +0100

    Update SEO.js

--- components/globals/SEO.js
+++ components/globals/SEO.js
@@ -10,6 +10,7 @@ export default function SEO({ description, title, image, slug }) {
 			<meta charSet="utf-8" />
 			<title>{`${title} | ${originalTitle}`}</title>
 			<meta name="description" content={`${description ? description : originalDescription}`} />
+			 <meta name="image" content={image} />
 			<meta property="og:type" content="website" />
 			<meta property="og:title" content={`${title ? title : originalTitle}`} key="ogtitle" />
 			<meta
@@ -17,8 +18,11 @@ export default function SEO({ description, title, image, slug }) {
 				content={`${description ? description : originalDescription}`}
 				key="ogdesc"
 			/>
-			<meta property="twitter:card" content="summary" key="twcard" />
+			<meta property="twitter:card" content="summary_large_image" key="twcard" />
 			<meta name="twitter:creator" content={twitter} key="twhandle" />
+			<meta name="twitter:title" content={`${title ? title : originalTitle}`} key="twtitle" />
+			<meta name="twitter:description" content={`${description ? description : originalDescription}`} key="twdescription"/>
+			<meta name="twitter:image" content={image} key="twimage"/>
 			<meta property="og:url" content={`${currentURL}/${slug}`} key="ogurl" />
 			<meta property="og:image" content={image} key="ogimage" />
 			<meta property="og:site_name" content={siteName} key="ogsitename" />
